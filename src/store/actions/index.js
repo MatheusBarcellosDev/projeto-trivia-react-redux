a18c846dc706bbaeff835244f0260c5a3d790540
Felipe Castanheira <castanheirafc2@gmail.com>
2022-02-14 18:22:21 -0300

    feat: get token

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,19 +1,19 @@
 export const addPlayer = (value) => ({ type: 'ADD_PLAYER', value });
 export const playGame = (value) => ({ type: 'GAME', value });
+export const getToken = (value) => ({ type: 'GET_TOKEN', value });
+export const setLoading = () => ({ type: 'LOADING' });
 
-// export function fetchAPI(expense) {
-//   return async (dispatch) => {
-//     try {
-//       dispatch(requestAPI());
-//       const response = await fetch('https://economia.awesomeapi.com.br/json/all');
-//       const data = await response.json();
-//       if (expense) {
-//         dispatch(addExpense(expense, data));
-//       } else {
-//         dispatch(getCurrencies(data));
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-// }
+export function tokenThunk() {
+  return async (dispatch) => {
+    try {
+      dispatch(setLoading());
+      const response = await fetch('https://opentdb.com/api_token.php?command=request');
+      const data = await response.json();
+      dispatch(getToken(data.token));
+      localStorage.setItem('token', data.token);
+      dispatch(setLoading());
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
